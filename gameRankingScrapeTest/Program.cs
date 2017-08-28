10424b0f4c3de49857240aaefa1b51b90eb556ba
Jonathan <jlouie123@gmail.com>
2017-08-28 15:25:12 -0700

    load html, scrape 1 column, insert 1 row into SQL db

--- gameRankingScrapeTest/Program.cs
+++ gameRankingScrapeTest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using HtmlAgilityPack;
 
 namespace gameRankingScrapeTest
 {
@@ -10,8 +12,42 @@ namespace gameRankingScrapeTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("commit test");
+            Console.WriteLine("Press enter to scrape the gamerankings website.");
             Console.ReadLine();
+            HtmlWeb hw = new HtmlWeb();
+            HtmlDocument htmlDoc = hw.Load("https://www.gamerankings.com/browse.html");
+            HtmlNodeCollection tables = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"main_col\"]//div//div//table//tr//td//a");
+            var gameName = tables.Select(node => node.InnerText);
+            var gameName2 = "super mario";
+            var gameName4 = "zelda";
+            IEnumerable<string> gameName3 = gameName;
+            string sqlquery = "INSERT into dbo.GameRankings (gameName) VALUES ('";
+            sqlquery = sqlquery + string.Join("'), ('", gameName);
+            //sqlquery = string.Concat(sqlquery, "')");
+            sqlquery += "')'";
+            Console.WriteLine(sqlquery);
+
+            Console.WriteLine("\nPress a key to output the game rankings to SQL database.");
+            Console.ReadLine();
+            using (SqlConnection conn = new SqlConnection("Data Source=DESKTOP-5LVATAU\\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True"))
+            {
+                using (SqlCommand command = conn.CreateCommand())
+                {
+
+
+                    //command.CommandText = "INSERT into dbo.GameRankings (gameName) VALUES (@gameName)";
+                    //command.Parameters.AddWithValue("@gameName", gameName2);
+                    //command.Parameters.AddWithValue("@gameName", gameName4);
+                    command.CommandText = sqlquery;
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            Console.WriteLine("\nGame rankings have been saved in the SQL database.");
+            Console.ReadLine();
+            //change 1 test
+
         }
     }
 }
