b525ee264ea39db052e8a7fad47914335ffa7906
Jonathan <jlouie123@gmail.com>
2017-08-28 21:37:36 -0700

    Zip both names and ranks together

--- gameRankingScrapeTest/Program.cs
+++ gameRankingScrapeTest/Program.cs
@@ -17,13 +17,23 @@ namespace gameRankingScrapeTest
             Console.ReadLine();
             HtmlWeb hw = new HtmlWeb();
             HtmlDocument htmlDoc = hw.Load("https://www.gamerankings.com/browse.html");
-            HtmlNodeCollection tables = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"main_col\"]//div//div//table//tr//td//a");
-            var gameName = tables.Select(node => node.InnerText);
+            HtmlNodeCollection gameNameNode = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"main_col\"]//div//div//table//tr//td//a");
+            HtmlNodeCollection gameRankingNode = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"main_col\"]//div//div//table//tr//td/span//b");
+            var gameName = gameNameNode.Select(node => node.InnerText);
+            var gameRanking = gameRankingNode.Select(node => node.InnerText);
+            var gameName2 = "super mario";
             Console.WriteLine("List of game names:");
-            foreach(var name in gameName)
+
+            var columns = gameName.Join(gameRanking, name => name, ranking => ranking, (name, ranking) => new { Name = name, Ranking = ranking});
+            var columns2 = gameName.Concat(gameRanking);
+            var columns3 = gameName.Zip(gameRanking, (n, r) => new { Name = n, Ranking = r });
+            foreach (var entry in columns3)
             {
-                Console.WriteLine(name);
+                Console.WriteLine(entry.Name + " " + entry.Ranking);
             }
+            
+
+            Console.ReadLine();
             string connstring = "Data Source=DESKTOP-5LVATAU\\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True";
             string insertQuery = "INSERT INTO dbo.GameRankings (gameName) VALUES (@gameName)";
             using (SqlConnection conn = new SqlConnection(connstring))
